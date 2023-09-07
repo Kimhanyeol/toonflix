4afd6b04324ae37f0536184d85404d4fd803742d
garlic <garlic@naver.com>
2023-09-07 15:58:08 +0900

    #6 WEBTOON APP(5) FromJson & Recap & waitForWebToons

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,22 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/services/api_service.dart';
+import '../models/webtoon_model.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+
+  List<WebtoonModel> webtoons = [];
+  bool isLoding = true;
+
+  void waitForWebToons() async {
+    webtoons = await ApiService.getTodaysToons();
+    isLoding = false;
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    waitForWebToons();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print(webtoons);
+    print(isLoding);
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
         centerTitle: true,
-        foregroundColor: Colors.green, //글자 색
-        backgroundColor: Colors.white, //앱 바 색
-        elevation: 2, //앱 바 밑에 음영 사라지게 만드는 코드
-        title: Text("오늘의 웹툰",
-        style: TextStyle(
-          fontSize: 24,
-          fontWeight: FontWeight.w600,
-        )),
+        //글자 색
+        foregroundColor: Colors.green,
+        //앱 바 색
+        backgroundColor: Colors.white,
+        //앱 바 밑에 음영 사라지게 만드는 코드
+        elevation: 2,
+        title: Text(
+          "오늘의 웹툰",
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
       ),
     );
   }
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,17 +1,23 @@
+import 'dart:convert';
+
 import 'package:http/http.dart' as http;
+import 'package:toonflix/models/webtoon_model.dart';
 
-class ApiService{
-  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
-  final String today = "today";
+class ApiService {
+  static const String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
+  static const String today = "today";
 
-  void getTodaysToons()async{
+  static Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstance =[];
     final url = Uri.parse('$baseUrl/$today');
-    final response =  await http.get(url);
-    if(response.statusCode==200){
-      print(response.body);
-      return;
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final List<dynamic> webtoons = jsonDecode(response.body);
+      for(var webtoon in webtoons){
+        webtoonInstance.add( WebtoonModel.fromJson(webtoon));
+      }
+      return webtoonInstance;
     }
     throw Error();
   }
 }
-
